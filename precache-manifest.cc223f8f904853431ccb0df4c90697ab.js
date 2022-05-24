0aa5ecdc982884ece3f78158f2f205ef6588ea43
Olumide Jones <olumideyjones@yahoo.com>
2022-05-24 12:33:20 +0200

    Updates

---
+++ precache-manifest.cc223f8f904853431ccb0df4c90697ab.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "869dfe8e5169a0d59431",
+    "url": "/Lumi4God/Template-001/static/css/main.bc8870a8.chunk.css"
+  },
+  {
+    "revision": "869dfe8e5169a0d59431",
+    "url": "/Lumi4God/Template-001/static/js/main.869dfe8e.chunk.js"
+  },
+  {
+    "revision": "4f8e0f6bc1512367651c",
+    "url": "/Lumi4God/Template-001/static/js/1.4f8e0f6b.chunk.js"
+  },
+  {
+    "revision": "2899d4980f771e3c2a05",
+    "url": "/Lumi4God/Template-001/static/js/runtime~main.2899d498.js"
+  },
+  {
+    "revision": "55f251b0a9158b186f17cc5c8896ecb1",
+    "url": "/Lumi4God/Template-001/static/media/Gym-Logo-Graphics.55f251b0.jpg"
+  },
+  {
+    "revision": "653a46df050e88535246455413a510e5",
+    "url": "/Lumi4God/Template-001/index.html"
+  }
+];
